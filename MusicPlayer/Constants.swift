9a942c0b5f069b8b4b2b56740b40aa550fe84131
Madalin Savoaia <madalin.savoaia@endava.com>
2018-05-30 10:18:06 +0300

    Add shuffle button colors in constants

---
+++ MusicPlayer/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  MusicPlayer
+//
+//  Created by Madalin Savoaia on 30/05/2018.
+//  Copyright © 2018 Madalin Savoaia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    static let shuffleButtonEnabledColor: UIColor = #colorLiteral(red: 0.8528880477, green: 0, blue: 0, alpha: 1)
+    static let shuffleButtonDisabledColor: UIColor = #colorLiteral(red: 0.6666666865, green: 0.6666666865, blue: 0.6666666865, alpha: 1)
+}
+
